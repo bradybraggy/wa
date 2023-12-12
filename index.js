97f85374e36d43891f86262b78e30daff276cb39
bradybraggy <152207392+bradybraggy@users.noreply.github.com>
2023-12-12 17:29:08 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+const Discord = require("discord.js");
+const { Client, Intents, Permissions, Collection } = require("discord.js");
+const { Routes } = require("discord-api-types/v9");
+const { clientId, guildId, token } = require("./config.json");
+const fs = require("fs");
+const generated = new Set();
+const express = require('express')
+const app = express();
+const port = 3000
+
+app.get('/', (req, res) => res.send('Yo boi!!'))
+
+app.listen(port, () =>
+console.log(`Your app is listening a http://localhost:${port}`)
+);
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.commands = new Collection();
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.data.name, command);
+}
+
+client.once("ready", () => {
+  console.log("Ready!");
+});
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) return;
+
+  const command = client.commands.get(interaction.commandName);
+
+  if (!command) return;
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+client.login(token);
